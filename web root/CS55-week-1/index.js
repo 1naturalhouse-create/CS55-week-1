4982272700c8df4ab4c338cf6f07cfabb474668e
1naturalhouse-create <1naturalhouse@gmail.com>
2025-09-05 08:56:04 -0700

    Update index.js

--- web root/CS55-week-1/index.js
+++ web root/CS55-week-1/index.js
@@ -1,33 +1,22 @@
 // CS55-week-01
 
-//use http package (shared code) from node.js
-let myhttp = require("http");
+const http = require("http");
 
-//use http [package createServer()
-// that runs a web server
-let myserver = myhttp.createServer(
-    // createServer() uses our function to run when a request comes in 
-    function( myrequest, myresponse){ 
-        console.log( myrequest.url);
-        let mytext;
-        if ( myrequest.url === '/hello'){
-            mytext = "hows it going...";
+const server = http.createServer((req, res) => {
+    console.log(req.url);
 
-        } else {
-            mytext = "who is this";
-        }
-        let mytext;
-        if ( myrequest.url === '/omg'){
-            mytext = "For Reals";
+    let mytext = "";
 
-        } else {
-            mytext = "Not for reals";
-        }
-       
+    if (req.url === "/hello") {
+        mytext = "hows it going...";
+    } else if (req.url === "/omg") {
+        mytext = "For Reals";
+    } else {
+        mytext = "who is this";
     }
-);
 
-// ask http to start listening on a tcp for incoming http request comes in
-// listen() takes 2 args: 1: tcp port #, string of the ip address to listen ( 0.0.0.0)
-myserver.listen(8080,"127.0.0.1");
- 
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(mytext);
+});
+
+server.listen(8080, "127.0.0.1");
